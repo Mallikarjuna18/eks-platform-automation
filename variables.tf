290dd818c5ea768345dfa2db9994e8f4b1eb4a13
Mallikarjuna18 <114546480+Mallikarjuna18@users.noreply.github.com>
2026-08-07 10:31:38 +0530

    default values addition

--- variables.tf
+++ variables.tf
@@ -22,16 +22,19 @@ variable "eks_version" {
 variable "vpc_cidr" {
   description = "VPC CIDR"
   type        = string
+  default     = "192.168.0.0/16"
 }
 
 variable "private_subnet_cidrs" {
   description = "Private subnet CIDRs"
   type        = list(string)
+  default     = ["192.168.1.0/24", "192.168.2.0/24"]
 }
 
 variable "public_subnet_cidrs" {
   description = "Public subnet CIDRs"
   type        = list(string)
+  default     = ["192.168.4.0/24", "192.168.5.0/24"]
 }
 variable "node_group_name" {
   description = "EKS Node Group Name"
