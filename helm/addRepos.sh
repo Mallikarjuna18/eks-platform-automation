42ec6c04b515f2346d375cee050e69bddb8d58bf
Mallikarjuna18 <114546480+Mallikarjuna18@users.noreply.github.com>
2026-08-07 16:38:50 +0530

    Uncomment argo repo addition in addRepos.sh

--- helm/addRepos.sh
+++ helm/addRepos.sh
@@ -7,7 +7,7 @@ helm repo add eks https://aws.github.io/eks-charts
 helm repo add autoscaler https://kubernetes.github.io/autoscaler
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add istio https://istio-release.storage.googleapis.com/charts
-#helm repo add argo https://argoproj.github.io/argo-helm
+helm repo add argo https://argoproj.github.io/argo-helm
 
 echo "Updating repositories..."
 helm repo update
