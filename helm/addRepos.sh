7b54643befee40f817ba6978656807bc2ca8b0de
Mallikarjuna18 <114546480+Mallikarjuna18@users.noreply.github.com>
2026-08-07 11:28:26 +0530

    Add autoscaler repo and comment out argo repo
    Added autoscaler Helm repository and commented out argo repository.

--- helm/addRepos.sh
+++ helm/addRepos.sh
@@ -3,11 +3,11 @@
 set -e
 
 echo "Adding Helm repositories..."
-
 helm repo add eks https://aws.github.io/eks-charts
+helm repo add autoscaler https://kubernetes.github.io/autoscaler
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add istio https://istio-release.storage.googleapis.com/charts
-helm repo add argo https://argoproj.github.io/argo-helm
+#helm repo add argo https://argoproj.github.io/argo-helm
 
 echo "Updating repositories..."
 helm repo update
